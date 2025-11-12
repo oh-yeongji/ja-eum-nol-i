7190b16ee1eb55a80c51004f3edcdeefe411c8ef
dev.cappp <dev.cappp@gmail.com>
2025-11-13 07:36:51 +0900

    style: gameScreen modal창 grardient작업

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const startBtn = document.querySelector(".startBtn");
-const gameScreen = document.querySelector("gameScreen");
-
+const gameScreen = document.querySelector(".gameScreen");
+const dim = document.querySelector(".dim");
 startBtn.onclick = function () {
   gameScreen.style.display = "block";
+  dim.style.display = "block";
+};
+
+dim.onclick = function () {
+  gameScreen.style.display = "none";
+  dim.style.display = "none";
 };
