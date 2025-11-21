dc0a12b62955e9a614b61c44733bee1d70301d46
dev.cappp <dev.cappp@gmail.com>
2025-11-21 23:08:11 +0900

    feat: gameSettings icon 설정

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const startBtn = document.querySelector(".startBtn");
 const gameScreen = document.querySelector(".gameScreen");
 const dim = document.querySelector(".dim");
 const chosungLoad = document.querySelector(".chosungLoad");
+
 startBtn.onclick = function () {
   gameScreen.style.display = "block";
   dim.style.display = "block";
@@ -12,15 +13,4 @@ dim.onclick = function () {
   dim.style.display = "none";
 };
 
-chosungLoad.onclick = function () {
-  const apiKey = "YOUR_API_KEY";
-  const word = "사랑";
-  const url = `https://opendict.korean.go.kr/api/search?key=${apiKey}&q=${encodeURIComponent(
-    word
-  )}&req_type=json`;
 
-  fetch(url)
-    .then((res) => res.text())
-    .then(console.log)
-    .catch(console.error);
-};
