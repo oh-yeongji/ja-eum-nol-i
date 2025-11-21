c4c5b7ec8455e9ab05d80381833f3155459dfcc0
dev.cappp <dev.cappp@gmail.com>
2025-11-22 02:22:24 +0900

    feat: gameSettings click할때 css작업

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const startBtn = document.querySelector(".startBtn");
-const gameScreen = document.querySelector(".gameScreen");
 const dim = document.querySelector(".dim");
+const gameSettings = document.querySelector(".gameSettings");
+const gameSettingsIcon = document.querySelector(".gameSettings > i");
+// 시간설정
+const timeSetting = document.querySelector(".timeSetting  i");
+// 게임 설명
+const gameInfo = document.querySelector(".gameInfo  i");
+const gameScreen = document.querySelector(".gameScreen");
 const chosungLoad = document.querySelector(".chosungLoad");
 
 startBtn.onclick = function () {
@@ -13,4 +19,21 @@ dim.onclick = function () {
   dim.style.display = "none";
 };
 
+gameSettings.addEventListener("click", function () {
+ 
+  gameSettings.style.width = "150px";
+  gameSettings.style.borderRadius = "20px";
+  // 눌렀을때 톱니
+  gameSettingsIcon.style.top = "25px";
+  gameSettingsIcon.style.left = "25px";
 
+  timeSetting.style.position = "absolute";
+  timeSetting.style.display = "block";
+  timeSetting.style.top = "20%";
+  timeSetting.style.left = "40%";
+
+  gameInfo.style.position = "absolute";
+  gameInfo.style.display = "block";
+  gameInfo.style.top = "20%";
+  gameInfo.style.left = "75%";
+});
