3e063fb237e60f00bcbab10d3bd45961370efef0
dev.cappp <dev.cappp@gmail.com>
2025-11-22 22:29:09 +0900

    feat: gameSettings창과 timeModal창 충돌 해결

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const dim = document.querySelector(".dim");
 const gameSettings = document.querySelector(".gameSettings");
 const gameSettingsIcon = document.querySelector(".gameSettings > i");
 // 시간설정
-const timeSetting = document.querySelector(".timeSetting  i");
+const timeSetting = document.querySelector(".timeSetting > i");
 // 시간설정모달창
 const timeModal = document.querySelector(".timeModal");
 
@@ -11,7 +11,7 @@ const timeModal = document.querySelector(".timeModal");
 const timeModalCloseBtn = document.querySelector(".closeBtn");
 
 // 게임 설명
-const gameInfo = document.querySelector(".gameInfo  i");
+const gameInfo = document.querySelector(".gameInfo > i");
 const gameScreen = document.querySelector(".gameScreen");
 const chosungLoad = document.querySelector(".chosungLoad");
 
@@ -25,7 +25,13 @@ dim.onclick = function () {
   dim.style.display = "none";
 };
 
-gameSettings.addEventListener("click", function () {
+gameSettings.addEventListener("click", (e) => {
+  // 시간 설정 아이콘 눌렀을 때는 gear 토글 방지
+  if (e.target.closest(".timeSetting")) return;
+
+  // 게임 설명 아이콘 눌렀을 때도 gear 토글 방지
+  if (e.target.closest(".gameInfo")) return;
+
   gameSettings.classList.toggle("expanded");
 });
 
