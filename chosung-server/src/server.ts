10863f004028aeefdb57a9dd5f112660bb5fbe63
dev.cappp <dev.cappp@gmail.com>
2025-12-23 23:55:01 +0900

    feat: define room state and room data structure

--- chosung-server/src/server.ts
+++ chosung-server/src/server.ts
@@ -11,6 +11,27 @@ const io = new Server(httpServer, {
   },
 });
 
+/*========================
+        
+         방 상태 설정
+
+==========================*/
+type RoomStaus = "WAIT" | "READY" | "COUNTDOWN" | "PLAY" | "RESULT";
+
+interface Player {
+  socketId: string;
+  userId: string;
+  nickname: string;
+}
+
+interface Room {
+  status: RoomStaus;
+  players: Map<string, Player>;
+  countdownTimer?: NodeJS.Timeout; 
+}
+
+const rooms = new Map<string, Room>();
+
 httpServer.listen(5173, () => {
   console.log("제대로 접속");
 });
