ca9256c6eabbb002cdcc3fff26727924b3c4d2c0
dev.cappp <dev.cappp@gmail.com>
2025-12-26 23:55:56 +0900

    feat: scaffold join-room event

--- chosung-server/src/server.ts
+++ chosung-server/src/server.ts
@@ -13,7 +13,7 @@ const io = new Server(httpServer, {
 
 /*========================
         
-         방 상태 설정
+         방 상태, 플레이어 설정
 
 ==========================*/
 type RoomStaus = "WAIT" | "READY" | "COUNTDOWN" | "PLAY" | "RESULT";
@@ -27,11 +27,28 @@ interface Player {
 interface Room {
   status: RoomStaus;
   players: Map<string, Player>;
-  countdownTimer?: NodeJS.Timeout; 
+  countdownTimer?: NodeJS.Timeout;
 }
 
 const rooms = new Map<string, Room>();
 
-httpServer.listen(5173, () => {
+const createRoom = (roomId: string): Room => {
+  const room: Room = {
+    status: "WAIT",
+    players: new Map(),
+  };
+  rooms.set(roomId, room);
+  return room;
+};
+
+io.on("connection", (socket: Socket) => {
+  console.log("connected:", socket.id);
+
+  socket.on("join-room", () => {
+    console.log("들어왔다.");
+  });
+});
+
+httpServer.listen(3000, () => {
   console.log("제대로 접속");
 });
