7cbea5efa67ddb98fb5f3d73e46b8b81cd5a3ebf
dev.cappp <dev.cappp@gmail.com>
2026-01-25 20:51:04 +0900

    chore: move validation messages to server side

--- chosung-server/src/game/gameService.ts
+++ chosung-server/src/game/gameService.ts
@@ -1,33 +1,63 @@
-import { extractFirstTwoChosungs } from "./chosung";
+import { extractTwoChosungs } from "./chosung";
+import { Room } from "../types";
+import { checkWordDetail } from "../lib/dict";
 
-export interface Room {
+interface ValidateParams {
   chosungPair: [string, string];
+  word: string;
+  usedWords: Set<string>;
 }
 
+export async function validateWord({
+  chosungPair,
+  word,
+  usedWords,
+}: ValidateParams) {
+  console.log(" NEW VALIDATEWORD CALLED!!!!!");
+  const trimmed = word.trim();
 
-export function validateWordByChosung(
-  word: string,
-  room: Room
-): {
-  valid: boolean;
-  reason?: string;
-} {
-  const chosung = extractFirstTwoChosungs(word);
+  if (!trimmed || trimmed === "") {
+    return {
+      valid: false,
+      reason: "단어를 입력하세요.",
+    };
+  }
 
+  if (usedWords.has(trimmed)) {
+    return {
+      valid: false,
+      reason: "이미 사용한 단어입니다.",
+    };
+  }
+  //word는입력값 유저가 보낸 단어에서 초성을 추출
+  const extracted = extractTwoChosungs(trimmed);
 
-
-  if (!chosung) {
+  if (!extracted) {
     return { valid: false, reason: "초성이 추출되지않음." };
   }
 
-  if (
-    chosung[0] !== room.chosungPair[0] ||
-    chosung[1] !== room.chosungPair[1]
-  ) {
+  if (extracted[0] !== chosungPair[0] || extracted[1] !== chosungPair[1]) {
+    return { valid: false, reason: "입력한 단어와 초성이 일치하지않습니다." };
+  }
+
+  const dictResult = await checkWordDetail(trimmed);
+  console.log("DICT RESULT RAW:", dictResult);
+  const { exist, isNoun } = dictResult;
+  console.log("exist:", exist, "isNoun:", isNoun);
+  if (!exist) {
     return {
       valid: false,
-      reason: "입력한 단어와 초성이 일치하지않습니다.",
+      reason: "사전에 없는 단어입니다.",
     };
   }
-  return { valid: true };
+
+  if (!isNoun) {
+    return {
+      valid: false,
+      reason: "명사가 아닙니다.",
+    };
+  }
+
+
+  return { valid: true, word: trimmed };
 }
