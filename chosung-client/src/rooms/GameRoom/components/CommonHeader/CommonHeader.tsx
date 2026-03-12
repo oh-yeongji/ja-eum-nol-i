9838494a8ccb6299bd294cf2ea4a20b1d8250ba8
oh-yeongji <dev.cappp@gmail.com>
2026-03-12 21:51:02 +0900

    feat: implement disabled state for CommonHeader component

--- chosung-client/src/rooms/GameRoom/components/CommonHeader/CommonHeader.tsx
+++ chosung-client/src/rooms/GameRoom/components/CommonHeader/CommonHeader.tsx
@@ -4,12 +4,22 @@ interface CommonHeaderProps {
   title: string;
   style?: React.CSSProperties;
   onClose?: () => void;
+  isCloseDisabled?: boolean;
 }
-const CommonHeader = ({ title, style, onClose }: CommonHeaderProps) => {
+const CommonHeader = ({
+  title,
+  style,
+  onClose,
+  isCloseDisabled,
+}: CommonHeaderProps) => {
   return (
     <div className={styles.header} style={style}>
       <span className={styles.titleName}>{title}</span>
-      <button className={styles.closeBtn} onClick={onClose}>
+      <button
+        className={styles.closeBtn}
+        onClick={onClose}
+        disabled={isCloseDisabled}
+      >
         X
       </button>
     </div>
